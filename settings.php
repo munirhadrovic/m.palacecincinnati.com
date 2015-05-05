1516d679b9d3afb6646487571e800ce48fb8ceb2
Emir <emiratesbp@gmail.com>
2015-05-05 09:23:56 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="601 Vine St"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Cincinnati, OH 45202";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=51338130000;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flow"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
